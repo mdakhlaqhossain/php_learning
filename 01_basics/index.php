a6f322b52f9592f76bbf9a080052427ee870492c
Md. Akhlaq Hossain <70303638+mdakhlaqhossain@users.noreply.github.com>
2024-10-08 16:05:39 +0000

    In-progress

--- 01_basics/index.php
+++ 01_basics/index.php
@@ -4,8 +4,26 @@
   </head>
   <body>
     <?php 
-      $food="Pizza";
-      echo '<h1>I Love '.$food.'</h1>';; 
+      echo 'Hello World';
+      //this is one line comment
+      /*
+      this is blocked line comment
+      this is blocked line comment
+      */
+      // PHP is partially case-sensitive.
+      echo '<h1>Hello World1</h1>';
+      ECHO '<h2>Hello World2</h2>';
+      Echo '<h3>Hello World3</h3>';
+    
+    
+      // PHP is case-sensitive except variable
+      // variable starts with $
+      // variable name start with letter or underscore
+      // variable name can contain letter, number, underscore
+      $food = "Pizza";
+      $Food = "Burger";
+      echo 'I Love '.$food.'<br>'; //string concat with .
+      echo 'I Love '.$Food;
     ?> 
 
 </html>
